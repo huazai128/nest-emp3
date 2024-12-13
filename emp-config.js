91f7601c7e923baf4d9415da7adf6834b381c02b
zhouweihua <zhouweihua@joyy.com>
2024-12-13 17:27:29 +0800

    feat： 基础框架

--- emp-config.js
+++ emp-config.js
@@ -2,8 +2,12 @@ import {defineConfig} from '@empjs/cli'
 import ReactPlugin from '@empjs/plugin-react'
 import {pluginRspackEmpShare} from '@empjs/share'
 import { join, resolve } from 'path'
+const InlineCodePlugin = require('html-inline-code-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const { TypedCssModulesPlugin } = require('typed-css-modules-webpack-plugin')
 
 export default defineConfig(store => {
+  const env = process.env.EMP_ENV = store.env || 'dev'
   return {
     plugins: [
       ReactPlugin(),
@@ -16,6 +20,18 @@ export default defineConfig(store => {
         },
       }),
     ],
+    server: {
+      port: 8008,
+      devMiddleware: {
+        index: true,
+        mimeTypes: {
+          phtml: 'text/html',
+        },
+        publicPath: './dist/client',
+        serverSideRender: true,
+        writeToDisk: true,
+      },
+    },
     build: {
       staticDir: '.',
       outDir: join(__dirname, './dist/client'),
@@ -32,6 +48,49 @@ export default defineConfig(store => {
       template: resolve('./views/index.html'),
       filename: resolve('./dist/views/index.html'),
       title: '基础架构框架',
+    },
+    chain(chain) {
+      if (env !== 'dev' && !!env) {
+        chain.devtool('source-map')
+        chain.plugin('SourcemapUploadPlugin').use(
+          new UploadSourceMapPlugin({
+            url: `http:localhost:5005/api/upload-zip`, // 上传url
+            uploadPath: resolve(__dirname, './dist/client/js'),
+            patterns: [/\js.map$/],
+            requestOption: {
+              data: {
+                siteId: '63b6568f66704eb3458306d6',
+              },
+            },
+          }),
+        )
+      }
+      chain.plugin('InlineCodePlugin').use(
+        new InlineCodePlugin({
+          begin: false,
+          tag: 'script',
+          inject: 'body',
+          code: `window.INIT_DATA = <%- JSON.stringify(data) %>`,
+        }),
+      )
+
+      // chain.plugin('CopyWebpackPlugin').use(
+      //   new CopyWebpackPlugin({
+      //     patterns: [
+      //       {
+      //         from: resolve(__dirname, './public'),
+      //         to: resolve(__dirname, './dist/views'),
+      //       },
+      //     ],
+      //   }),
+      // )
+
+      chain.plugin('TypedCssModulesPlugin').use(
+        new TypedCssModulesPlugin({
+          globPattern: 'src/!(styles)/**/*.scss',
+        }),
+      )
+
     },
   }
 })
