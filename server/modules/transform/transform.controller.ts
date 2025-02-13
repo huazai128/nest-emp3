57010d7e6f0696c52ea00832dd4279cb9ceb5996
zhouweihua <zhouweihua@joyy.com>
2025-02-13 21:49:11 +0800

    feat: 优化代码

--- server/modules/transform/transform.controller.ts
+++ server/modules/transform/transform.controller.ts
@@ -4,14 +4,27 @@ import { Responsor } from '@app/decorators/responsor.decorator';
 import { TransformPipe } from '@app/pipes/transform.pipe';
 import { HttpRequest } from '@app/interfaces/request.interface';
 import { QueryParams } from '@app/decorators/params.decorator';
+import { createLogger } from '@app/utils/logger';
+
+const logger = createLogger({
+  scope: 'transform',
+  time: true,
+});
 
 @Controller('api')
 export class TransferController {
   constructor(private readonly transferService: TransferService) {}
 
+  /**
+   * Get 接口转发
+   * @param {HttpRequest} data 请求参数,经过TransformPipe处理
+   * @return {Promise<any>} 转发后的响应结果
+   * @memberof TransferController
+   */
   @Responsor.api()
   @Get('transform')
   getTransform(@QueryParams('query', new TransformPipe()) data: HttpRequest) {
+    logger.info('getTransform', data);
     return this.transferService.get(data);
   }
 
@@ -24,6 +37,7 @@ export class TransferController {
   @Responsor.api()
   @Post('transform')
   postTransform(@Body(new TransformPipe()) data: HttpRequest) {
+    logger.info('postTransform', data);
     return this.transferService.post(data);
   }
 }
