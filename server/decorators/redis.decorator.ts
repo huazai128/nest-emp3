2c0d81966ae7badbeb047b560cf4d9255b59589d
zhouweihua <zhouweihua@joyy.com>
2025-02-21 17:18:00 +0800

    feat: 备注代码

--- server/decorators/redis.decorator.ts
+++ server/decorators/redis.decorator.ts
@@ -4,10 +4,18 @@ import {
 } from '@app/processors/redis/redis.util';
 import { Inject } from '@nestjs/common';
 
+/**
+ * Redis连接装饰器
+ * @returns Inject装饰器,注入Redis连接实例
+ */
 export const InjectRedis = () => {
   return Inject(getRedisConnectionToken());
 };
 
+/**
+ * Redis配置装饰器
+ * @returns Inject装饰器,注入Redis配置选项
+ */
 export const InjectRedisOptions = () => {
   return Inject(getRedisOptionsToken());
 };
